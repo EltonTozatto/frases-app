da8138f3635a7b6b438f71747b9cc0d3989cbc2c
Elton Tozatto <eltontozatto@gmail.com>
2023-01-26 20:40:59 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/com/eltontozatto/frases/ui/incluirfrase/IncluirFraseActivity.kt
@@ -0,0 +1,12 @@
+package br.com.eltontozatto.frases.ui.incluirfrase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.eltontozatto.frases.R
+
+class IncluirFraseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_incluir_frase)
+    }
+}
