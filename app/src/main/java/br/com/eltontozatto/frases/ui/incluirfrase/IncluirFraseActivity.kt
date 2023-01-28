2e6cffe0f070e43c5eba5ffefd9579e178082e9c
Elton Tozatto <eltontozatto@gmail.com>
2023-01-28 16:02:03 -0300

    Criado listeners para botão Cancelar e Salvar e validado os campos da Activity

--- app/src/main/java/br/com/eltontozatto/frases/ui/incluirfrase/IncluirFraseActivity.kt
+++ app/src/main/java/br/com/eltontozatto/frases/ui/incluirfrase/IncluirFraseActivity.kt
@@ -2,11 +2,53 @@ package br.com.eltontozatto.frases.ui.incluirfrase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.eltontozatto.frases.R
+import br.com.eltontozatto.frases.databinding.ActivityIncluirFraseBinding
 
 class IncluirFraseActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIncluirFraseBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_incluir_frase)
+        binding = ActivityIncluirFraseBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        configurarListeners()
+    }
+
+    private fun configurarListeners() {
+        configurarListenerCancelarButton()
+        configurarListenerSalvarButton()
+    }
+
+    private fun configurarListenerSalvarButton() {
+        binding.apply {
+            btnSalvar.setOnClickListener {
+                val autor = autorFrasesET.text.toString()
+                val frase = frasesET.text.toString()
+
+                autorFrasesTIL.error = if (autor.isEmpty())
+                    getString(R.string.err_sem_autor)
+                else
+                    null
+
+                frasesTIL.error = if (frase.isEmpty())
+                    getString(R.string.err_sem_frase)
+                else
+                    null
+
+                if (autor.isNotEmpty() && frase.isNotEmpty()) {
+                    Toast.makeText(applicationContext, "Autor: $autor Frase: $frase", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun configurarListenerCancelarButton() {
+        binding.btnCancelar.setOnClickListener {
+            finish()
+        }
     }
 }
