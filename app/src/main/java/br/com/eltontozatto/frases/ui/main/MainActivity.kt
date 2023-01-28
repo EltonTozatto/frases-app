e479ef5d9ed85a5701efc5a12efce5ddd1037aaf
Elton Tozatto <eltontozatto@gmail.com>
2023-01-28 15:47:08 -0300

    Ajustando margens e configurando o click do Float Action Button

--- app/src/main/java/br/com/eltontozatto/frases/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/eltontozatto/frases/ui/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package br.com.eltontozatto.frases.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.eltontozatto.frases.databinding.ActivityMainBinding
+import br.com.eltontozatto.frases.ui.incluirfrase.IncluirFraseActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,5 +14,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        configurarListeners()
+    }
+
+    private fun configurarListeners() {
+        configurarFabListeners()
+    }
+
+    private fun configurarFabListeners() {
+        binding.fabAdicionarFrase.setOnClickListener {
+            startActivity(Intent(this, IncluirFraseActivity::class.java))
+        }
     }
 }
